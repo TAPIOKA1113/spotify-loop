b91b85c823a2c48f3213ca4e2ddab26ad3ce6de5
TAPIOKA1113 <manato20021113@gmail.com>
2024-12-28 14:19:53 +0900

    pwa化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+  react(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['assets/favicon.ico'],
+    manifest: {
+      name: 'Spotify LOOP',
+      short_name: 'Spotify LOOP',
+      description: 'Spotify LOOP',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'assets/192_sample_img.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'assets/512_sample_img.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+  ]
+});
